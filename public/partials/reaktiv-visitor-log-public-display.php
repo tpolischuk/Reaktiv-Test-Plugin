e9de38f305270d31d1712635ef77efaf331df939
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-06-26 21:22:57 -0700

    Adding a custom page template w/ the plugin 	- Moved the form out of the activator

--- public/partials/reaktiv-visitor-log-public-display.php
+++ public/partials/reaktiv-visitor-log-public-display.php
@@ -11,4 +11,31 @@
  * @package    Reaktiv_Visitor_Log
  * @subpackage Reaktiv_Visitor_Log/public/partials
  */
-?>
+
+ get_header();
+ ?>
+
+ 	<section id="primary" class="content-area">
+ 		<main id="main" class="site-main">
+
+      <div class="form-container">
+        <form id="visitor-login-form" action="<?php echo esc_url( admin_url('admin-post.php') ) ?>">
+      <h3>Visitor Registration Form</h3>
+      <input type="text" required placeholder="Your Name" name="guest" />
+
+      <input type="email" required placeholder="Your E-mail" name="email" />
+
+      <h1>Put employee data here</h1>
+      <select required name="host">
+
+      </select>
+
+      <input type="hidden" name="action" value="visitor_login_form">
+      <input id="visitor-submit" type="submit" value="Submit" />
+      </form></div>
+
+ 		</main><!-- #main -->
+ 	</section><!-- #primary -->
+
+ <?php
+ get_footer();
