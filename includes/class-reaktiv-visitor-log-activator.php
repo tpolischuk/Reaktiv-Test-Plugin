7f465521a69ecd4bf1fee3a3a2386425f9a868bd
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-06-27 14:21:44 -0700

    Adding employee data to new table in DB

--- includes/class-reaktiv-visitor-log-activator.php
+++ includes/class-reaktiv-visitor-log-activator.php
@@ -20,11 +20,51 @@
 */
 class Reaktiv_Visitor_Log_Activator {
 	/**
-	* TODO update this to reflect the new functionality
+	* Creates a custom table and inserts the employee data.
+	* Creates a custom page at /visit/.
 	*
 	* @since    1.0.0
 	*/
 	public static function activate() {
+
+		// Create a database table for storing the employee data
+
+		global $wpdb;
+
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE reaktiv_visitor_log_employees (
+				id mediumint(9) NOT NULL AUTO_INCREMENT,
+				name varchar(55) NOT NULL,
+				desk mediumint(9) NOT NULL,
+				UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		if ( ! function_exists('dbDelta') ) {
+				require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		}
+
+		dbDelta( $sql );
+
+		add_option( 'reaktiv_visitor_log_db_version', '1.0' );
+
+		// Retrieve the employee data from the remote JSON file
+		$employee_url = "https://gist.githubusercontent.com/jjeaton/21f04d41287119926eb4/raw/4121417bda0860f662d471d1d22b934a0af56eca/coworkers.json";
+
+		$response = wp_remote_get( $employee_url );
+		if ( is_array( $response ) ) {
+			$body = $response['body'];
+		}
+
+		$employee_data = json_decode($body);
+
+		//Insert the data into the custom table
+		foreach ($employee_data as $employee) {
+			$wpdb->insert("reaktiv_visitor_log_employees",array(
+				"name" => $employee->name,
+				"desk"=>$employee->desk
+			));
+		};
 
 		$generated_visitor_page = array(
 			'post_title'    => wp_strip_all_tags( 'Visit' ),
