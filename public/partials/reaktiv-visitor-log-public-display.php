fda37d65a4fc229527f824d48fb2fcd70635d7cd
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-06-26 21:25:52 -0700

    Indentation for the new page template

--- public/partials/reaktiv-visitor-log-public-display.php
+++ public/partials/reaktiv-visitor-log-public-display.php
@@ -1,41 +1,42 @@
 <?php
 
 /**
- * Provide a public-facing view for the plugin
- *
- * This file is used to markup the public-facing aspects of the plugin.
- *
- * @link       http://www.trevorpolischuk.com
- * @since      1.0.0
- *
- * @package    Reaktiv_Visitor_Log
- * @subpackage Reaktiv_Visitor_Log/public/partials
- */
+* Provide a public-facing view for the plugin
+*
+* This file is used to markup the public-facing aspects of the plugin.
+*
+* @link       http://www.trevorpolischuk.com
+* @since      1.0.0
+*
+* @package    Reaktiv_Visitor_Log
+* @subpackage Reaktiv_Visitor_Log/public/partials
+*/
 
- get_header();
- ?>
+get_header();
+?>
 
- 	<section id="primary" class="content-area">
- 		<main id="main" class="site-main">
+<section id="primary" class="content-area">
+  <main id="main" class="site-main">
 
-      <div class="form-container">
-        <form id="visitor-login-form" action="<?php echo esc_url( admin_url('admin-post.php') ) ?>">
-      <h3>Visitor Registration Form</h3>
-      <input type="text" required placeholder="Your Name" name="guest" />
+    <div class="form-container">
+      <form id="visitor-login-form" action="<?php echo esc_url( admin_url('admin-post.php') ) ?>">
+        <h3>Visitor Registration Form</h3>
+        <input type="text" required placeholder="Your Name" name="guest" />
 
-      <input type="email" required placeholder="Your E-mail" name="email" />
+        <input type="email" required placeholder="Your E-mail" name="email" />
 
-      <h1>Put employee data here</h1>
-      <select required name="host">
+        <h1>Put employee data here</h1>
+        <select required name="host">
 
-      </select>
+        </select>
 
-      <input type="hidden" name="action" value="visitor_login_form">
-      <input id="visitor-submit" type="submit" value="Submit" />
-      </form></div>
+        <input type="hidden" name="action" value="visitor_login_form">
+        <input id="visitor-submit" type="submit" value="Submit" />
+      </form>
+    </div>
 
- 		</main><!-- #main -->
- 	</section><!-- #primary -->
+    </main><!-- #main -->
+  </section><!-- #primary -->
 
- <?php
- get_footer();
+  <?php
+  get_footer();
